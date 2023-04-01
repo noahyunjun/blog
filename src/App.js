e483aced3ed2f39b08bae2ee43dfb8e589ba143d
HamHyeonJun <hyunjun003@naver.com>
2023-04-01 15:40:47 +0900

    feat: first commit - 모달 동적 생성까지 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { createPortal } from "react-dom";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [글제목, set글제목] = useState(["다", "가", "나"]);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState([false, false, false]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: "red", fontSize: "16px" }}>ReactBlog</h4>
+        {/* style에 넣을때는 object 형태로 넣어야한다. */}
+      </div>
+
+      {글제목.map((param, param2) => {
+        return (
+          <div className="list">
+            <h4
+              onClick={() => {
+                let modalCopy = [...modal];
+                modalCopy[param2] = !modalCopy[param2];
+                setModal(modalCopy);
+              }}
+            >
+              {param}
+            </h4>
+            <span
+              onClick={() => {
+                let copy = [...like];
+                copy[param2] = copy[param2] + 1;
+                setLike(copy);
+              }}
+            >
+              🔥{like[param2]}
+            </span>
+
+            <p>3월 30일 발행</p>
+            {modal[param2] == true ? (
+              <Modal 글제목={글제목} setTitle={set글제목} index={param2} />
+            ) : (
+              ""
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <h4>{props.글제목[props.index]}</h4>
+      <p>3월 30일 발행</p>
+      <p>detail</p>
+      <button
+        onClick={() => {
+          let 글제목Copy = [...props.글제목];
+          글제목Copy[props.index] = "수정된 타이틀";
+          props.setTitle(글제목Copy);
+        }}
+      >
+        글수정
+      </button>
+    </div>
+  );
+};
 
 export default App;

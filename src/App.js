52fbd53918c4e71a527c1b4ca372c62321c2668a
HamHyeonJun <hyunjun003@naver.com>
2023-04-05 20:30:29 +0900

    feat: class 문법 및 버그 수정 - 마지막 글만 삭제되는 현상 픽스

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { createPortal } from "react-dom";
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   let [글제목, set글제목] = useState(["다", "가", "나"]);
@@ -14,10 +14,9 @@ function App() {
         <h4 style={{ color: "red", fontSize: "16px" }}>ReactBlog</h4>
         {/* style에 넣을때는 object 형태로 넣어야한다. */}
       </div>
-
       {글제목.map((param, param2) => {
         return (
-          <div className="list">
+          <div className="list" key={param2}>
             <h4
               onClick={() => {
                 let modalCopy = [...modal];
@@ -44,9 +43,9 @@ function App() {
               ""
             )}
             <button
-              onClick={(e) => {
+              onClick={() => {
                 let listCopy = [...글제목];
-                listCopy.pop(e.target.value);
+                listCopy.splice(param2, 1);
                 set글제목(listCopy);
               }}
             >
@@ -72,6 +71,7 @@ function App() {
       >
         submit
       </button>
+      <Modal2></Modal2>
     </div>
   );
 }
@@ -94,5 +94,33 @@ const Modal = (props) => {
     </div>
   );
 };
+
+class Modal2 extends React.Component {
+  //constructor, super, render 무조건 들어가야 합니다
+  constructor(props) {
+    //props는 여기에서 받음.
+    super(props);
+    this.state = {
+      //state는 object 형태로 선언
+      name: "ham",
+      age: 20,
+    };
+  }
+  render() {
+    // this.props 라고 받는다.
+    return (
+      <div>
+        안녕 {this.state.age}
+        <button
+          onClick={() => {
+            this.setState({ age: 21 });
+          }}
+        >
+          age
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
